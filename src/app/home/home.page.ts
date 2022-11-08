2f36b0474c759b47643336396d5a9cd9197d7720
Larry Allen <larryallen@Larrys-MacBook-Pro.local>
2022-11-08 14:03:31 -0500

    add images for a loop

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,8 @@
 import { AfterContentChecked, Component, ViewChild, ViewEncapsulation } from '@angular/core';
 import { SwiperComponent } from 'swiper/angular';
-import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
+import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, SwiperOptions, Zoom, EffectCube } from 'swiper';
 
-SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom]);
+SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, EffectCube]);
 
 
 @Component({
@@ -17,6 +17,20 @@ export class HomePage implements AfterContentChecked {
   touchAllowed = false;
 
   constructor() {}
+
+  config: SwiperOptions = {
+    pagination: true,
+    slidesPerView: 'auto',
+    effect: 'cube',
+  }
+ 
+  images = [
+    'https://images.unsplash.com/photo-1580894742597-87bc8789db3d',
+    'https://images.unsplash.com/photo-1580927752452-89d86da3fa0a',
+    'https://images.unsplash.com/photo-1498050108023-c5249f4df085',
+    'https://images.unsplash.com/photo-1461749280684-dccba630e2f6',
+    'https://images.unsplash.com/photo-1488229297570-58520851e868'
+  ];
  
   ngAfterContentChecked(): void {
     if (this.swiper) {
