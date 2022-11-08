e45f206fa12cff42f66e1056c5f7e8eb5b49b721
Larry Allen <larryallen@Larrys-MacBook-Pro.local>
2022-11-08 11:20:13 -0500

    added simple text slider

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { SwiperComponent } from 'swiper/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class HomePage {
+export class HomePage implements AfterContentChecked {
+  @ViewChild('swiper') swiper: SwiperComponent;
 
   constructor() {}
-
+ 
+  ngAfterContentChecked(): void {
+    if (this.swiper) {
+      this.swiper.updateSwiper({});
+    }
+  }
 }
